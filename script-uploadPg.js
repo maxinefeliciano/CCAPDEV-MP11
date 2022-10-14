d2645258ccc0eb0370f60e654d53b7fd474fa634
maxinefeliciano <a.maxine.feliciano@gmail.com>
2022-10-14 12:30:53 +0800

    NavBar fixed

--- script-uploadPg.js
+++ script-uploadPg.js
@@ -3,6 +3,26 @@ const uploadBox = document.getElementById("upload-box");
 const thumbnailTxt = document.getElementById("upload-text");
 const uploadIcon = document.getElementById("upload-icon");
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
 // Virtually clicks the real-file button
 uploadBox.addEventListener("click", () => {
     realFileBtn.click();
